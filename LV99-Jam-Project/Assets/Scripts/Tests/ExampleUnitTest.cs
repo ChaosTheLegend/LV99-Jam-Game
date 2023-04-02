b165126c605a861e3404ce97187495ab623b411e
ChaosTheLegend <chaosthelegend@inbox.ru>
2023-04-02 18:55:38 +0600

    Initial Commit

---
+++ LV99-Jam-Project/Assets/Scripts/Tests/ExampleUnitTest.cs
@@ -0,0 +1,12 @@
+using Zenject;
+using NUnit.Framework;
+
+[TestFixture]
+public class ExampleUnitTest : ZenjectUnitTestFixture
+{
+    [Test]
+    public void RunTest1()
+    {
+        // TODO
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LV99-Jam-Game
 Repository for LV99 GameJam
+
+### Conventions:
+
+Source Unity Project is stored in LV99-Jam-Project folder 
